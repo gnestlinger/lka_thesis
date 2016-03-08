db98c862c3ff03d5298689abfa5f36fda00c6148
georgne <georgne@5c18669f-4f01-0010-a393-25170347a25b>
2016-03-08 11:47:44 +0000

    Comments revised.

--- lka/trunk/parameter/paramFile_SingleTrackMdl_BMW5_var1.m
+++ lka/trunk/parameter/paramFile_SingleTrackMdl_BMW5_var1.m
@@ -15,15 +15,15 @@
 about.vehicle = 'BMW 5 - Variation';
 about.source = 'IPG Carmaker 4.0';
 
-m = 1564;       % Fahrzeugmasse [kg]
-Iz = 2230;      % Trägheitsmoment um die Hochachse [kgm^2]
+m = 1564;		% Fahrzeugmasse [kg]
+Iz = 2230;		% Trägheitsmoment um die Hochachse [kg*m^2]
 
-lv = 1.268;     % Schwerpunktlage [m]
-lh = 1.620;     % Schwerpunktlage [m]
-l = lv+lh;      % Achsabstand [m]
-                
-csv = 2*70000;  % Schräglaufsteifigkeit vorne [N/rad]
-csh = 2*70000;  % Schräglaufsteifigkeit hinten [N/rad]
+lv = 1.268;		% Schwerpunktlage/Forderachse [m]
+lh = 1.620;		% Schwerpunktlage/Hinterachse [m]
+l = lv+lh;		% Achsabstand [m]
+
+csv = 2*70000;	% Schräglaufsteifigkeit vorne [N/rad]
+csh = 2*70000;	% Schräglaufsteifigkeit hinten [N/rad]
 
 
 %%% Parametervariation
--- lka/trunk/parameter/paramFile_SingleTrackMdl_Smart_MCC.m
+++ lka/trunk/parameter/paramFile_SingleTrackMdl_Smart_MCC.m
@@ -15,25 +15,25 @@
 about.vehicle = 'MCC-Smart';
 about.source = 'FTG-LV';
 
-m = 820;        % Fahrzeugmasse [kg]
-Iz = 3000;      % Trägheitsmoment um die Hochachse [kgm^2]
+m = 820;		% Fahrzeugmasse [kg]
+Iz = 3000;		% Trägheitsmoment um die Hochachse [kg*m^2]
 
-lv = 1.142;     % Schwerpunktlage [m]
-lh = 0.670;     % Schwerpunktlage [m]
-l = lv+lh;      % Achsabstand [m]
-                
-csv = 70000;    % Schräglaufsteifigkeit vorne [N/rad]
-csh = 90000;    % Schräglaufsteifigkeit hinten [N/rad]
+lv = 1.142;		% Schwerpunktlage/Forderachse [m]
+lh = 0.670;		% Schwerpunktlage/Hinterachse [m]
+l = lv+lh;		% Achsabstand [m]
 
-vmax = 150/3.6; % maximale Geschwindigkeit [m/s]
+csv = 70000;	% Schräglaufsteifigkeit vorne [N/rad]
+csh = 90000;	% Schräglaufsteifigkeit hinten [N/rad]
 
-% is = 15.3;      % Lenkübersetzung
-% 
-% b = 1.5;        % Spurweite [m]
-% h = 0.56;       % Schwerpunkthöhe [m]
+vmax = 150/3.6;	% maximale Geschwindigkeit [m/s]
+
+% is = 15.3;		% Lenkübersetzung
+%
+% b = 1.5;		% Spurweite [m]
+% h = 0.56;		% Schwerpunkthöhe [m]
 % hsp = 0.46;
 % h0 = 0.1;
-% 
-% Ix = 730;       % Trägheitsmoment [kgm^2]
-% cx = 70000;     % Wankfedersteifigkeit [Nm/rad]
-% dx = 6000;      % Wankdämpfungskonstante [Nms/rad]
+%
+% Ix = 730;		% Trägheitsmoment [kgm^2]
+% cx = 70000;		% Wankfedersteifigkeit [Nm/rad]
+% dx = 6000;		% Wankdämpfungskonstante [Nms/rad]
