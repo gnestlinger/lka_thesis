cdd34fe1994968923080a68d44b5f63dbc3f49e8
Georg Nestlinger <84226458+georgathome@users.noreply.github.com>
2017-03-13 20:28:16 +0000

    Method doPlot: renamed to plotLaneTracking; order of input arguments changed so that it can also be called without the inputs of transformed street segment and transformed CG origin; input handling added.

--- trajectories/segDat.m
+++ trajectories/segDat.m
@@ -360,14 +360,14 @@ classdef segDat
 			% error if no element of 'logIndx' is true
 			if ~any(any(logIndx))
 % 				obj.doPlot(obj,obj_T,xyCG_global,xyCG_T,yawAngle_global,lad,numIndx);
-				obj.doPlot(obj,obj_T,xyCG_global,xyCG_T,yawAngle_global,lad,[indRow,indCol]);
+				obj.plotLaneTracking(obj,xyCG_global,yawAngle_global,lad,indCol,obj_T,xyCG_T);
 				error('segDat:laneTracking',...
 					['Keine Elemente der Sollbahn im Bereich der',... 
 					' aktuellen Fahrzeugposition gefunden'])
 			end%if
 			
 % 			obj.doPlot(obj,obj_T,xyCG_global,xyCG_T,yawAngle_global,lad,numIndx);
-			obj.doPlot(obj,obj_T,xyCG_global,xyCG_T,yawAngle_global,lad,[indRow,indCol]);
+			obj.plotLaneTracking(obj,xyCG_global,yawAngle_global,lad,indCol,obj_T,xyCG_T);
 			
 			%%% get lateral offset for potential elements
 			% interpolation index-range: values>1 mainly increase the
@@ -390,7 +390,7 @@ classdef segDat
 				end%for
 			catch exception
 				disp(exception.message);
-				obj.doPlot(obj,obj_T,xyCG_global,xyCG_T,yawAngle_global,lad,[indRow,indCol]);
+				obj.plotLaneTracking(obj,xyCG_global,yawAngle_global,lad,indCol,obj_T,xyCG_T);
 			end%try
 			
 			
@@ -980,46 +980,93 @@ classdef segDat
 		end%fcn
 		
 		
-		function doPlot(sd_global,sd_T,xyCG_global,xyCG_T,psi_global,lad,indx)
+		function plotLaneTracking(...
+				sd_global,xyCG_global,psi_global,LAD,indx,...
+				sd_T,xyCG_T)
 			
-			% Solltrajektorie (global)
-			h = plottangent(sd_global,indx(:,2));
+			
+			%%% handle input arguments
+			% number of input arguments
+			if nargin ~= 5 && nargin ~= 7
+				error('Invalid number of input arguments!');
+			end%if
+			
+			% XYCG_global
+			xyCG_global = xyCG_global(:);
+			if any(size(xyCG_global) ~= [2 1])
+				error('Input argument XYCG_GLOBAL must be a vector of two elements!');
+			end%if
+			
+			% PSI_GLOBAL
+			if ~isscalar(psi_global)
+				error('Input argument PSI_GLOBAL must be a scalar!');
+			end%if
+			
+			% LAD
+			LAD = LAD(:);
+			if ~isvector(LAD)
+				error('Input argument LAD must be a vector!');
+			end%if
+			
+			
+			%%% start plotting
+			% desired path (global)
+			h = plottangent(sd_global,indx);
 			set(h(1),...
 				'LineStyle','none',...
 				'Marker','o',...
 				'MarkerSize',3,...
 				'MarkerFaceColor','b');
 			
-			% Fahrzeug (global)
+			% vehicle representation (global)
 			hold on
 			plot(xyCG_global(1),xyCG_global(2),...
 				'ob',...
 				'MarkerSize',7,...
 				'MarkerFaceColor','b');
-			Fzglachse = xyCG_global + max(lad)*[cos(psi_global); sin(psi_global)];
+			Fzglachse = xyCG_global + max(LAD)*[cos(psi_global); sin(psi_global)];
 			plot([xyCG_global(1),Fzglachse(1)],[xyCG_global(2),Fzglachse(2)],...
 				'b',...
 				'LineWidth',2)
 			
 			
-			% Solltrajektorie (transformiert)
-			h_V = plottangent(sd_T,indx(:,2),'k');
-			set(h_V(1),...
-				'Color','g',...
-				'LineStyle','none',...
-				'Marker','x',...
-				'MarkerSize',5,...
-				'MarkerFaceColor','r');
+			if nargin > 5
+				
+				%%% handle input arguments
+				% SD_T
+				if ~isa(sd_T,'segDat')
+					error('Input argument SD_T must be of class SEGDAT!');
+				end%if
+				
+				% XYCG_T
+				xyCG_T = xyCG_T(:);
+				if any(size(xyCG_T) ~= [2 1])
+					error('Input argument XYCG_T must be a vector of two elements!');
+				end%if
+				
+				
+				%%% start plotting
+				% Solltrajektorie (transformiert)
+				h_V = plottangent(sd_T,indx,'k');
+				set(h_V(1),...
+					'Color','g',...
+					'LineStyle','none',...
+					'Marker','x',...
+					'MarkerSize',5,...
+					'MarkerFaceColor','r');
+				
+				% Fahrzeug (transformiert)
+				hold on
+				plot(xyCG_T(1),xyCG_T(2),...
+					'rx','MarkerSize',7,...
+					'MarkerFaceColor','r');
+				Fzglachse_T = xyCG_T + [max(LAD);0];
+				plot([xyCG_T(1),Fzglachse_T(1)],[xyCG_T(2),Fzglachse_T(2)],...
+					'r',...
+					'LineWidth',2)
 			
-			% Fahrzeug (transformiert)
-			hold on
-			plot(xyCG_T(1),xyCG_T(2),...
-				'rx','MarkerSize',7,...
-				'MarkerFaceColor','r');
-			Fzglachse_T = xyCG_T + [max(lad);0];
-			plot([xyCG_T(1),Fzglachse_T(1)],[xyCG_T(2),Fzglachse_T(2)],...
-				'r',...
-				'LineWidth',2)
+			end%if
+			
 			hold off
 			grid on
 			axis auto
