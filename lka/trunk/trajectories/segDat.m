4649e64c4223c4e5a7f9602869ab54642eb7bfea
georgne <georgne@5c18669f-4f01-0010-a393-25170347a25b>
2017-04-01 19:07:13 +0000

    Method TEST_PLOTDIFF revised.

--- lka/trunk/trajectories/segDat.m
+++ lka/trunk/trajectories/segDat.m
@@ -1250,20 +1250,19 @@ classdef segDat
 		end%fcn
 		
 		
-		function test_plotdiff()
+		function test_plotdiff(sd)
 			
 			close all
-			a = lkaSegmentStraight([],200,0);
-			b = lkaSegmentClothoid([],0,0.01,a.segmentData.phi(end),200);
-			c = lkaSegmentCircle([],b.segmentData.phi(end)-pi/2,pi*1/2,300);
-			d = lkaSegmentClothoid([],0.01,0,c.segmentData.phi(end),200);
-			e = lkaSegmentStraight([],200,d.segmentData.phi(end));
+			if nargin < 1
+				a = lkaSegmentStraight([],200,0);
+				b = lkaSegmentClothoid([],0,0.01,a.segmentData.phi(end),200);
+				c = lkaSegmentCircle([],b.segmentData.phi(end)-pi/2,pi*1/2,300);
+				d = lkaSegmentClothoid([],0.01,0,c.segmentData.phi(end),200);
+				e = lkaSegmentStraight([],200,d.segmentData.phi(end));
+				sd = a + b + c + d + e;
+			end%if
 			
-			sd = a + b + c + d + e;
-			
-			tic
 			plotdiff(sd);
-			toc
 			
 		end%fcn
 		
