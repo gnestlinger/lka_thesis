5138ae97f4dd7f23474a7f356b15563adaf175c1
georgne <georgne@5c18669f-4f01-0010-a393-25170347a25b>
2021-05-15 10:06:53 +0000

    Cleanup

--- lka/trunk/misc/deg2rad.m
+++
@@ -1,8 +0,0 @@
-function [ output_args ] = deg2rad( input_args )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-    output_args = input_args.*pi/180;
-
-end
-
--- lka/trunk/misc/rad2deg.m
+++
@@ -1,8 +0,0 @@
-function [ output_args ] = rad2deg( input_args )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-    output_args = input_args.*180/pi;
-
-end
-
